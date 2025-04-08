90a7f5b6d1bbf9625dec9a18f55bf7a1bb82a144
Juan Garces <juanuniestudio2403@gmail.com>
2025-04-08 16:33:34 -0500

    Update s3-taller.c

--- s3-taller.c
+++ s3-taller.c
@@ -6,6 +6,9 @@ int main (int argc, char *argv[]) {
 
     char estudiante[4][30];
     char materia[2][30];
+    float notas[2][4];
+
+    do{
 
      int opc1;
 
@@ -17,8 +20,46 @@ int main (int argc, char *argv[]) {
      printf("5. Promedio por asignatura.\n");
      printf("6. Notas Maxima y Minima.\n");
      printf("7. Aprobados y Reprobados\n");
+     printf("8. Salir...\n");
+    printf(">> ");
+     scanf(" %d",&opc1);
+    switch (opc1)
+    {
+    case 1:
+        printf("Ingrese las 3 materias: \n");
+        
+        for(int i=0; i <= 2 ; i++)
+        {
+            fflush(stdin);
+            printf("Materia #%i:\n", i+1);
+            fgets(materia[i], 30, stdin);
+        }
 
+        break;
+    case 2:
+        printf("Ingrese los 5 estudiantes\n");
 
+        for(int j=0; j <= 4; j++)
+        {
+            fflush(stdin);
+            printf("Estudiante #%i: ", j+1);
+            fgets(estudiante[j], 30, stdin);
+        
+        }
+
+        break;
+    case 3:
+        
+    printf("#\t\tMateria\n");
+    for(int i = 0; i < cont; i++)
+            {
+                printf("%d\t\t%s\t\t%s\t\t%d\t\t%.2f\n",i,id[i],nombre[i],stock[i],precio[i]);
+            }
+
+    break;
+    default:
+        break;
+    }
 
 
 
