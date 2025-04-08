b3b7c004160cc37067f02a440aff086c0f4173ca
Juan Garces <juanuniestudio2403@gmail.com>
2025-04-08 16:06:01 -0500

    Create s3-taller.c

---
+++ s3-taller.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char *argv[]) {
+
+    char estudiante[4][30];
+    char materia[2][30];
+
+     int opc1;
+
+     printf("Programa de Gestion de notas\n");
+     printf("1. Ingresar Materia.\n");
+     printf("2. Ingresar Estudiantes.\n");
+     printf("3. Ingresar Notas.\n");
+     printf("4. Promedio por estudiantes.\n");
+     printf("5. Promedio por asignatura.\n");
+     printf("6. Notas Maxima y Minima.\n");
+     printf("7. Aprobados y Reprobados\n");
+
+
+
+
+
+
+
+    return 0;
+}
